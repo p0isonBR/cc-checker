d1d6928bf0effe494b07f2c428e71becab7ace8d
p0isonBR <65384918+p0isonBR@users.noreply.github.com>
2020-11-01 23:28:58 -0300

    Update cc-checker.py

--- cc-checker.py
+++ cc-checker.py
@@ -1,4 +1,62 @@
-import requests,json
+import os,time,requests,json
+
+#Cores
+R="\033[1;31m"; B="\033[1;34m"; C="\033[1;37m"; Y="\033[1;33m"; G="\033[1;32m"; RT="\033[;0m"
+
+os.system('clear')
+
+print(C+"                            /+osyhhhhhhyys++/")
+print("                         +oydddhhhhyyhhhhdddhy+/")
+print("                      /+yddhyyyys.josue.syyhddhs/")
+print("                     +hddyyssssssssssssssssssyyhdds/")
+print("                   /sddhyyyyyyssssssssssssssyyyyyhmh+")
+print("                  /hmdhhddddddhhhyyyyyyhhhhdddddhhhddo")
+print("                 /hmmdhs+/ //+osyhhhhhhysso+////ohddmdo")
+print("                /hmmmy"+B+".           `````          `"+C+"smddd+")
+print("                smddm/"+B+"     `````          `````   "+C+"mdhmh/")
+print("               +ddydm+"+B+"  -/osyyyys+.    ./syyyyso/-"+C+"mdydms")
+print("               ymhyhmh"+B+".yyo/ -- +hdo  /dho -- /oyh."+C+"ymdyymd/")
+print("              /dmyyymd"+B+".  ``.-   ./   -/.-   .``  `"+C+"dmhsydmo")
+print("              smdysymd"+B+"   shdhyydy      sdyyhddy   "+C+"dmyyshmy")
+print("              dmysshmy"+B+"                            "+C+"smhssymd/")
+print("             /dmyssymd"+B+"                            "+C+"hmhsyymm/")
+print("             /dmyssyhms"+B+"                          /"+C+"mdysyymm/")
+print("             /dmyssyydm/"+B+"  sh       hh/     -hy  ."+C+"dmyssyymm/")
+print("              dmhssssydd/"+B+" -hdhysshdysdhssyhdd  -"+C+"hmhyssyymd/")
+print("              ymhssssyyddo"+B+"``. //+/.` ./+// -` /"+C+"ddhysssyhmh")
+print("              +mdysssssyhdh"+B+" `     `/+`      -"+C+"sddysssssydmo")
+print("               ymhysssssyyddh/"+B+"`   `dm.   ` "+C+"sddhysssssyhmh/")
+print("               /hmhysssssyyyhdds"+B+" ..dm . "+C+"ohddhyyssssyyhmd+")
+print("                /yddhyssssssyyhhddhddddddhyyssssssyydmh+")
+print("               /+sdmmdhhyyyysssyyyyyyyyyysssyyyyyhddmdyo+/")
+print("           /+shdddhhyhhddddddhhhhhhhhhhhhhhdddddddhyyhhdddyo/")
+print("         /shddhyyysssssyyyyhhhhhhhhhhhhhhhhhhyyyyyssssyyyhdddy+")
+print("        /hmhyyssssssssssssssssssssssssssssssssssssssssssssyyhddo")
+print("        /dmhyyyyyssssssssssssssssssssssssssssssssssssssyyyyyydms")
+print("         +yhddddddhhhhhyyyyyyyyyyyyyyyyyyyyyyyyyyhhhhhdddddddhs/")
+print("           //++oossyyhhhhhhhdddddddddddddddddhhhhhhyyssoo++///")
+print("                       ///////+++++++++++++//////")
+print("                        ___  _                 ___   ____")
+print("                       / _ \(_)               |  _ \|  __ \ ")
+print("                 ____ | | | | |___   _   ____ | |_| | |__) |")
+print("                |  _ \| | | | / __|/ _ \|  _ \| |_ <|  _  /")
+print("                | |_) | |_| | \__ \ (_) | | | | |_| ) | \ \ ")
+print("                |  __/ \___/|_|___/\___/|_| |_|____/|_|  \_\ ")
+print(C+"                | |"+RT+B+"*t.me/p0isonBR*"+RT)
+print(C+"                |_|"+RT+B+"*by p0isonBR"+RT)
+
+time.sleep(3); os.system('clear')
+
+print(B+"*By PoisonBR"+RT+G+"""
+ $$$$$$\   $$$$$$\     $$$$$$\  $$\                           $$\                           
+$$  __$$\ $$  __$$\   $$  __$$\ $$ |                          $$ |                          
+$$ /  \__|$$ /  \__|  $$ /  \__|$$$$$$$\   $$$$$$\   $$$$$$$\ $$ |  $$\  $$$$$$\   $$$$$$\  
+$$ |      $$ |$$$$$$\ $$ |      $$  __$$\ $$  __$$\ $$  _____|$$ | $$  |$$  __$$\ $$  __$$\ 
+$$ |      $$ |\______|$$ |      $$ |  $$ |$$$$$$$$ |$$ /      $$$$$$  / $$$$$$$$ |$$ |  \__|
+$$ |  $$\ $$ |  $$\   $$ |  $$\ $$ |  $$ |$$   ____|$$ |      $$  _$$<  $$   ____|$$ |      
+\$$$$$$  |\$$$$$$  |  \$$$$$$  |$$ |  $$ |\$$$$$$$\ \$$$$$$$\ $$ | \$$\ \$$$$$$$\ $$ |      
+ \______/  \______/    \______/ \__|  \__| \_______| \_______|\__|  \__| \_______|\__|      
+                                                                                       v 1.0"""+C+"""
 
 try:
     def checker(cc,mes,ano,cvv):
